d1d2b2c3c5b4e95021eec46c74264f177a2cc72a
Amineafilal <amineafilalelalami@gmail.com>
2024-02-24 07:27:59 -0800

    11. Zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - creates an infinite loop to keep the program running
+ * Return: 0 on success
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+			sleep(1); // Give time for the child process to become a zombie
+		}
+		else
+		{
+			exit(0);
+		}
+	}
+
+	infinite_while();
+
+	return (0);
+}
